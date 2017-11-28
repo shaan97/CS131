48ac56e798f5c461614f5d6f73d6f11c6008a1e2
Shaan Mathur <shaankaranmathur@gmail.com>
2017-11-27 17:14:17 -0800

    Test suite

--- hw3/test.sh
+++ hw3/test.sh
@@ -3,22 +3,26 @@
 echo "Compiling..."
 javac UnsafeMemory.java
 
-printf "Synchronized\n"
-for ((j=1;j<=32;j*=2))
-do
-	printf "Threads %d\n" $j
-	for ((i=1;i<=5;i+=1))
-	do
-		>&2 echo $i
-	
-		java UnsafeMemory Synchronized $j 100000000 127 50 20 58 25 75 | sed 's/[^0-9.]*//g'
-	done
-done
 
 printf "Synchronized\n"
-for ((j=1;j<=32;j*=2))
+for ((j=1;j<=8;j*=2))
 do
-        printf "Threads %d\n" $j
+    $sum=0
+    printf "Threads %d\n" $j
+        for ((i=1;i<=5;i+=1))
+        do
+                >&2 echo $i
+
+                java UnsafeMemory Synchronzied $j 100000000 127 50 20 58 25 75 | s\
+ed 's/[^0-9.]*//g'
+        done
+done
+
+printf "BetterSafe\n"
+for ((j=1;j<=8;j*=2))
+do
+    $sum=0
+    printf "Threads %d\n" $j
         for ((i=1;i<=5;i+=1))
         do
                 >&2 echo $i
